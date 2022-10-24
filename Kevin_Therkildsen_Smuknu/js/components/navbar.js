c174b0fe6c17dece23e84dbafe23bec9d2caecc9
Pucinokt <pucinokt@hotmail.com>
2022-10-24 17:14:16 +0200

    update
    update

--- Kevin_Therkildsen_Smuknu/js/components/navbar.js
+++ Kevin_Therkildsen_Smuknu/js/components/navbar.js
@@ -7,9 +7,9 @@ navigation.innerHTML += `
       <nav id="menu">
         <ul>
         <li><a href="index.html">Forside</a></li>
-        <li><a href="#">Vores Produkter</a></li>
-        <li><a href="#">Spørg om sundhed</a></li>
-        <li><a href="contact.html">Bliv Medlem</a></li>
+        <li><a href="produkter.html">Vores Produkter</a></li>
+        <li><a href="sundhed.html">Spørg om sundhed</a></li>
+        <li><a href="medlem.html">Bliv Medlem</a></li>
         </ul>
       </nav>
       <div onclick="menuShow()" id="burger">
@@ -17,9 +17,7 @@ navigation.innerHTML += `
         <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
       </svg>
     </div>
-      
     </div>
- 
   </header>
 `
 
