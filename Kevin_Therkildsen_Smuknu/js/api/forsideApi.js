c679be6042f683d0dd484258593ef1cb2b04c20d
Pucinokt <pucinokt@hotmail.com>
2022-10-26 14:57:23 +0200

    update
    update

--- Kevin_Therkildsen_Smuknu/js/api/forsideApi.js
+++ Kevin_Therkildsen_Smuknu/js/api/forsideApi.js
@@ -8,6 +8,8 @@ Promise.all([fetch("https://smuknu.webmcdm.dk/products",
 })
 
 // her har jeg lavet et for loop, hvor jeg har valgt at bruge ternary(?) og conditional(:) operator for at tjekke om strengen er tom "". Hvis den er tom "" så fortæller jeg at den skal være tom igen "". Men ellers så skal der være en værdi af "discountInPercent" fra API'en i <p>Spar %</p>. <----- Dette sker EFTER conditional (:)
+
+// i forhold til recommended har jeg sagt den skal starte fra "0" og loop igennem hele for-løkken. Jeg har valgt at bruge ".length" i stedet for at definere et tal fordi det er mere dynamisk
 .then(function(FP){
     return FP.json()
     })
@@ -16,8 +18,8 @@ Promise.all([fetch("https://smuknu.webmcdm.dk/products",
         for(let j=0; j < FP.length; j++)
         !0==FP[j].recommended && (F+=
             `<div class='Products'>
-                <img class='Products-image' src='${FP[j].image}'>${""==FP[j].discountInPercent?"":
-            `<div class='Product-Discount'><h1>Spar</h1><br><p>${FP[j].discountInPercent}%</p></div>`}
+                <img class='Products-image' src='${FP[j].image}'>${""==FP[j].discountInPercent?"": /* <---- Jeg tjekker om strengen fra id'et discountInPercent er tomt. Hvis dette er TRUE så SKAL strengen være tom "". Efter den tomme streng kommer vores conditional operator, der siger at hvis værdien er FALSE skal tallet fra discountInPercent i REST API'en printes ud i browseren */
+            `<div class='Product-Discount'><h1>Spar</h1><p>${FP[j].discountInPercent}%</p></div>`}
 
              <div class='Products-details'>
                 <h3 class='Products-title'>${FP[j].title}</h3>
--- Kevin_Therkildsen_Smuknu/js/api/produktApi.js
+++ Kevin_Therkildsen_Smuknu/js/api/produktApi.js
@@ -3,30 +3,32 @@
 
 Promise.all([fetch("https://smuknu.webmcdm.dk/products",
 
-{headers:
-    {Accept:"application/json"
-}
-})
-
-// her har jeg lavet et for loop, hvor jeg har valgt at bruge ternary(?) og conditional(:) operator for at tjekke om strengen er tom "". Hvis den er tom "" så fortæller jeg at den skal være tom igen "". Men ellers så skal der være en værdi af "discountInPercent" fra API'en i <p>Spar %</p>. <----- Dette sker EFTER conditional (:)
-
-.then(function(KT){
-    return KT.json()
+    {
+        headers:
+        {
+            Accept: "application/json"
+        }
     })
-    .then(function(KT){
-        let K="";
-        for(let I=0; I < KT.length; I++) (K+=
+
+    // her har jeg lavet et for loop, hvor jeg har valgt at bruge ternary(?) og conditional(:) operator for at tjekke om strengen er tom "". Hvis den er tom "" så fortæller jeg at den skal være tom igen "". Men ellers så skal der være en værdi af "discountInPercent" fra API'en i <p>Spar %</p>. <----- Dette sker EFTER conditional (:)
+
+    .then(function (KT) {
+        return KT.json()
+    })
+    .then(function (KT) {
+        let K = "";
+        for (let I = 0; I < KT.length; I++) (K +=
             `<div class='Products'>
-                <img class='Products-image' src='${KT[I].image}'>${""==KT[I].discountInPercent?"":
-                `<div class='Product-Discount'><h1>Spar</h1><br><p>${KT[I].discountInPercent}%</p></div>`}
+                <img class='Products-image' src='${KT[I].image}'>${"" == KT[I].discountInPercent ? "" :/* <---- Jeg tjekker om strengen fra id'et discountInPercent er tomt. Hvis dette er TRUE så SKAL strengen være tom "". Efter den tomme streng kommer vores conditional operator, der siger at hvis værdien er FALSE skal tallet fra discountInPercent i REST API'en printes ud i browseren */
+                `<div class='Product-Discount'><h1>Spar</h1><p>${KT[I].discountInPercent}%</p></div>`}
 
              <div class='Products-details'>
                 <h3 class='Products-title'>${KT[I].title}</h3>
                 <p class='Products-text'>${KT[I].price},00 kr.</p>
             </div>
             </div>`);
-          document.querySelector("#Products").innerHTML=K
-        }
-        ),
-    ]);
+        document.querySelector("#Products").innerHTML = K
+    }
+    ),
+]);
 // Products End
